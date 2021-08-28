b453fb62a7ddd101b507837683e9df4db2e7b369
Carlos <Carlos@DESKTOP-GHFQK5E>
2021-08-28 02:53:44 -0500

    Create model todo

---
+++ todoazure.Common/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace todoazure.Common.Models
+{
+    public class Todo
+    {
+        public string IdEmployee { get; set; }
+
+        public DateTime DateTimeEmployee { get; set; }
+
+        public int Type { get; set; }
+
+        public Boolean Consolidated { get; set; }
+    }
+}
