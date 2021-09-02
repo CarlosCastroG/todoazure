23ab3db554cd332bf2e22d4f0541c8d20d2ba86a
Carlos <Carlos@DESKTOP-GHFQK5E>
2021-09-02 06:33:27 -0500

    First correct unit test, finally :')

---
+++ todoazure.Test/Tests/TodoApiTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using todoazure.Common.Models;
+using todoazure.Functions.Functions;
+using todoazure.Test.Helpers;
+using Xunit;
+
+namespace todoazure.Test.Tests
+{
+    public class TodoApiTest
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+
+        [Fact]
+        public async void CreateTodo_Should_Return_200()
+        {
+            // Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoRequest);
+            // Act
+            IActionResult response = await TodoAPI.CreateTodo(request, mockTodos, logger);
+
+            // Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+
+        }
+    }
+}
