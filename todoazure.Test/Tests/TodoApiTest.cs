cd395d301c522740b0e39577624a761b7e604123
Carlos <Carlos@DESKTOP-GHFQK5E>
2021-09-05 14:32:59 -0500

    Test delete

--- todoazure.Test/Tests/TodoApiTest.cs
+++ todoazure.Test/Tests/TodoApiTest.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using todoazure.Common.Models;
+using todoazure.Functions.Entities;
 using todoazure.Functions.Functions;
 using todoazure.Test.Helpers;
 using Xunit;
@@ -21,6 +22,7 @@ namespace todoazure.Test.Tests
             MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
             Todo todoRequest = TestFactory.GetTodoRequest();
             DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoRequest);
+
             // Act
             IActionResult response = await TodoAPI.CreateTodo(request, mockTodos, logger);
 
@@ -28,6 +30,42 @@ namespace todoazure.Test.Tests
             OkObjectResult result = (OkObjectResult)response;
             Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
 
+        }
+
+        [Fact]
+        public async void UpdateTodo_Should_Return_200()
+        {
+            // Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId, todoRequest);
+
+            // Act
+            IActionResult response = await TodoAPI.UpdateTodo(request, mockTodos, todoId.ToString(), logger);
+
+            // Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+
+        [Fact]
+        public async void DeleteTodo_Should_Return_200()
+        {
+            // Arrenge
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Todo todoRequest = TestFactory.GetTodoRequest();
+            TodoEntity todoEntity = TestFactory.GetTodoEntity();
+            Guid todoId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(todoId, todoRequest);
+
+            // Act
+            IActionResult response = await TodoAPI.DeleteTodo(request, todoEntity, mockTodos, todoId.ToString(), logger);
+
+            // Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
         }
     }
 }
